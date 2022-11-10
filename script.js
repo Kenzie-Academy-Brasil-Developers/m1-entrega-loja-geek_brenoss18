d0290a19cca46d56e6e9f6e44cf09f7b8a7fc1e6
Breno <Brenopereira2022@gmail.com>
2022-11-10 15:08:06 -0300

    entrega-catalogo-geek

--- script.js
+++ script.js
@@ -16,66 +16,39 @@ separateItens(todosItens)
 // console.log(listFigures)
 // console.log(listFrames)
 
-function criarElemento(itens){
-    for (let i = 0; i < listFigures.length; i ++){
+function criarElemento(itens, ul){
+    for (let i = 0; i < itens.length; i ++){
      let divCriarCard = document.createElement('div')
       divCriarCard.className = 'card'
      let divtratarImg = document.createElement('div')
       divtratarImg.className = 'tratarImg'
       divCriarCard.appendChild(divtratarImg)
      let inserirImg = document.createElement('img')
-     inserirImg.src = listFigures[i].image
+     inserirImg.src = itens[i].image
      divtratarImg.appendChild(inserirImg)
      let pTitulo = document.createElement('p')
      pTitulo.className = 'nomeIten'
-     pTitulo.innerText = listFigures[i].name
+     pTitulo.innerText = itens[i].name
      divCriarCard.appendChild(pTitulo)
      let pValorIten = document.createElement('p')
      pValorIten.className = 'valorItem'
-     pValorIten.innerText = listFigures[i].price
+     pValorIten.innerText = itens[i].price
      divCriarCard.appendChild(pValorIten)
      
-     let selectList1 = document.querySelector(".list1")
+     
      console.log(selectList1)
      let criarLi = document.createElement('li')
      criarLi.appendChild(divCriarCard)
-     selectList1.appendChild(criarLi)
+     ul.appendChild(criarLi)
      let adicionarSecao1 = document.querySelector(".actions")
      adicionarSecao1.appendChild(selectList1)
 
     }
 }
-criarElemento(listFigures)
+let selectList1 = document.querySelector(".list1")
+let selectList2 = document.querySelector(".list2")
+criarElemento(listFigures, selectList1)
 
-function criarElemento2(itens){
-    for (let i = 0; i < listFrames.length; i ++){
-     let divCriarCard2 = document.createElement('div')
-      divCriarCard2.className = 'card'
-     let divtratarImg2 = document.createElement('div')
-      divtratarImg2.className = 'tratarImg'
-      divCriarCard2.appendChild(divtratarImg2)
-     let inserirImg2 = document.createElement('img')
-     inserirImg2.src = listFrames[i].image
-     divtratarImg2.appendChild(inserirImg2)
-     let pTitulo2 = document.createElement('p')
-     pTitulo2.className = 'nomeIten'
-     pTitulo2.innerText = listFrames[i].name
-     divCriarCard2.appendChild(pTitulo2)
-     let pValorIten2 = document.createElement('p')
-     pValorIten2.className = 'valorItem'
-     pValorIten2.innerText = listFrames[i].price
-     divCriarCard2.appendChild(pValorIten2)
-     
-     let selectList2 = document.querySelector(".list2")
-     console.log(selectList2)
-     let criarLi2 = document.createElement('li')
-     criarLi2.appendChild(divCriarCard2)
-     selectList2.appendChild(criarLi2)
-     let adicionarSecao2 = document.querySelector(".pictures")
-     adicionarSecao2.appendChild(selectList2)
-
-    }
-}
-criarElemento2(listFrames)
+criarElemento(listFrames, selectList2)
 
 
